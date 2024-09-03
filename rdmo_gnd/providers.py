49edb8ae9e995fd4fc8e84496b0927b62eeca9e5
Jochen Klar <mail@jochenklar.de>
2024-09-03 14:29:28 +0200

    Add user and site to get_options and add get_search

--- rdmo_gnd/providers.py
+++ rdmo_gnd/providers.py
@@ -1,3 +1,5 @@
+import re
+
 from django.conf import settings
 
 import requests
@@ -10,11 +12,13 @@ class GNDProvider(Provider):
     search = True
     refresh = True
 
-    def get_options(self, project, search=None):
+    def get_options(self, project, search=None, user=None, site=None):
         if search:
             url = getattr(settings, 'GND_PROVIDER_URL', 'https://lobid.org/gnd').rstrip('/')
             headers = getattr(settings, 'GND_PROVIDER_HEADERS', {})
-            response = requests.get(f'{url}/search', params={'q': search, 'format': 'json'}, headers=headers)
+            response = requests.get(f'{url}/search', params={
+                'q': self.get_search(search), 'format': 'json'
+            }, headers=headers)
 
             try:
                 data = response.json()
@@ -45,3 +49,11 @@ class GNDProvider(Provider):
             return f'{preferredName} [{gndIdentifier}]'
         else:
             return f'{preferredName} | {professionOrOccupation} [{gndIdentifier}]'
+
+    def get_search(self, search):
+        # reverse get_text to perform the search, remove everything after | or [
+        m = re.match(r'^[^|[]+', search)
+        if m:
+            return m[0].strip()
+        else:
+            return search
